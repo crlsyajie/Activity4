e5d4f96ccc44badd0162a3da4399d2dbbd168c9a
unknown <purcia2204@gmail.com>
2023-11-08 10:11:47 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -8,3 +8,15 @@ CREATE TABLE owners (
     phone VARCHAR(15),
     email VARCHAR(100)
 );
+
+CREATE TABLE animals (
+    animal_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    species VARCHAR(50),
+    breed VARCHAR(50),
+    date_of_birth DATE,
+    gender VARCHAR(10),
+    color VARCHAR(50),
+    owner_id INT,
+    FOREIGN KEY (owner_id) REFERENCES owners(owner_id)
+);
