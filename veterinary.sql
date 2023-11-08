1f1a58bb49e1ef4ecc364a80df30a08c083f72e8
Carlos Yajie Fetizanan <cyfetizanan@gmail.com>
2023-11-08 10:02:14 +0800

    feat/create-table-owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,10 @@
 create database veterinary;
+
+CREATE TABLE owners (
+    owner_id INT PRIMARY KEY,
+    ofirst_name VARCHAR(50),
+    olast_name VARCHAR(50),
+    address VARCHAR(100),
+    phone VARCHAR(15),
+    email VARCHAR(100)
+);
